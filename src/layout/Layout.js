129f214f0ffd2056ff52c311f0ca71af83a12181
srikanth <srikanthgummadi516@gmail.com>
2024-02-19 14:48:59 +0530

    removed spaces

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,12 +1,9 @@
-// import { useTheme } from '@mui/material/styles';
 import { Box } from '@mui/material';
 import Register from 'pages/authentication/Register';
 const Layout = () => {
-  // const theme = useTheme();
 
   return (
     <Box sx={{ display: 'flex', width: '100%' }}>
-
       <Box component="main" sx={{ width: '100%', flexGrow: 1, p: { xs: 2, sm: 3 } }}>
         <Register />
       </Box>
